3ef1d1ca9a968532e6785e40b1ae8a7ca6bc8a9d
SaurabhPasi28 <saurabhpasi123.sp@gmail.com>
2024-11-23 00:01:55 +0530

    config env file

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,21 +1,20 @@
 import axios from "axios";
-import { API_KEY} from "../config";
-const BASE_URL = "https://api.themoviedb.org/3"
+const BASE_URL = process.env.REACT_APP_BASE_URL; // Accessing from .env
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 
 export const fetchDataFromApi = async (url, params) => {
-  try {
-      const data = await axios.get(BASE_URL + url, {
-          params: {
-              api_key: API_KEY,
-              ...params,
-          },
-      });
-      console.log(data);
-      return data;
-  } catch (error) {
-      console.log(error);
-      return error;
-  }
-};
-
-
+    try {
+        const data = await axios.get(BASE_URL + url, {
+            params: {
+                api_key: API_KEY,
+                ...params,
+            },
+        });
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+  };
